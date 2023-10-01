f44ad5015dda89860ab9f1a57df331938df0fd70
4ndferreira <andferreira.art@outlook.com>
2023-10-01 15:20:31 -0300

    Refactored icons and style

--- Optional_Challenge/src/components/NavBar.tsx
+++ Optional_Challenge/src/components/NavBar.tsx
@@ -1,10 +1,22 @@
-const NavBar = () => {
-  return (
-    <nav className="flex justify-between h-[3.5rem] w-full p-4 max-w-md mx-auto">
-      <img src="assets/SearchIcon.svg" alt="" />
-      <img src="assets/MenuIcon.svg" alt="" />
-    </nav>
-  )
+import { MenuIcon, SearchIcon } from "./Icons";
+
+const styles = {
+  container: [
+    'flex', 
+    'justify-between', 
+    'h-[3.5rem]', 
+    'w-full', 
+    'p-4', 
+    'max-w-md', 
+    'mx-auto'
+  ].join(' ')
 }
 
-export default NavBar
+export default function NavBar() {
+  return (
+    <nav className={styles.container}>
+      <SearchIcon />
+      <MenuIcon />
+    </nav>
+  );
+}
